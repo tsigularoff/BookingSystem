39b4b734d5d92cf8485109cd37f12381dfdf845d
tsigularoff <tsigularoff@gmail.com>
2014-10-06 18:48:32 +0300

    HotelsController init, index.js - adding hotels controller

---
+++ server/controllers/HotelsController.js
@@ -0,0 +1,15 @@
+var Hotel = requre('mongoose').model('Hotel');
+
+function getAllHotels(req, res, next) {
+    Hotel.find({}).exec(function (err, collection) {
+        if(err){
+            throw "Hotels could not be loaded" + err;
+        }
+
+        res.send(collection);
+    })
+}
+
+module.exports = {
+    getAllHotels : getAllHotels
+};
