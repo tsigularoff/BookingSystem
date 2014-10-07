722c1a99f0551f37b9f77304203d39a0a642117e
Geno Genov <g.genov1993@gmail.com>
2014-10-07 20:50:24 +0300

    merge fix

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,20 +1,19 @@
 var usersController = require('../controllers/usersController');
 var coursesController = require('../controllers/coursesController');
 var hotelsController = require('../controllers/hotelsController');
-<<<<<<< HEAD
 var reservationsController=require('../controllers/ReservationsController');
-=======
+
 var roomsController = require('../controllers/roomsController');
->>>>>>> origin/master
+
 
 
 module.exports = {
     users: usersController,
     courses: coursesController,
     hotels: hotelsController,
-<<<<<<< HEAD
+
     reservations:reservationsController
-=======
+
     rooms: roomsController
->>>>>>> origin/master
+
 };
