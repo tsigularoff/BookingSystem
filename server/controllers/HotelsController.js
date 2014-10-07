a4ccfe8c6b259384f7f3c37bd85c3b25b990743b
Kiril Tsarnakliyski <elinos@gmail.com>
2014-10-08 00:34:16 +0300

    added delete methods for System.Object[] and users

--- server/controllers/HotelsController.js
+++ server/controllers/HotelsController.js
@@ -1,15 +1,15 @@
 var Hotel = require('mongoose').model('Hotel');
-var validators=require('../utilities/validators');
+var validators = require('../utilities/validators');
 
-var ALLOWED_SORT_HOTELS=['name']; //TODO: Fill
-var ITEMS_LIMIT=10;
+var ALLOWED_SORT_HOTELS = ['name']; //TODO: Fill
+var ITEMS_LIMIT = 10;
 
 function getAllHotels(req, res, next) {
 
     var page = validators.validatePage(req.query.page);
-    var sortBy = validators.validateSort(req.query.sortBy,ALLOWED_SORT_HOTELS);
+    var sortBy = validators.validateSort(req.query.sortBy, ALLOWED_SORT_HOTELS);
 
-    Hotel.find({}).skip((page-1)*ITEMS_LIMIT).limit(ITEMS_LIMIT).populate('owner').exec(function (err, collection) {
+    Hotel.find({}).skip((page - 1) * ITEMS_LIMIT).limit(ITEMS_LIMIT).populate('owner').exec(function (err, collection) {
         if (err) {
             throw "Hotels could not be loaded" + err;
         }
@@ -31,7 +31,7 @@ function createHotel(req, res, next) {
 
 function getHotelById(req, res, next) {
     Hotel.findOne({_id: req.params.id}).populate('owner').exec(function (err, hotel) {
-        if (err) {
+        if (err || !hotel) {
             console.log('Hotel could not be loaded: ' + err);
             res.status(400).json({message: err});
         }
@@ -40,8 +40,27 @@ function getHotelById(req, res, next) {
     })
 }
 
+function deleteHotel(req, res, next) {
+    Hotel.findOne({_id: req.params.id}).exec(function (err, hotel) {
+        if (err || !hotel) {
+            console.log('Hotel could not be deleted: ' + err);
+            res.status(400).json({message: err});
+        }
+        else if (req.user._id === hotel.owner._id || req.user.roles.indexOf('admin') > -1) {
+
+            hotel.remove(function () {
+                res.status(200).json({message: 'Hotel deleted!'});
+            });
+        }
+        else {
+            res.status(400).json({message: 'You do not have permissions!'});
+        }
+    });
+}
+
 module.exports = {
     getAllHotels: getAllHotels,
     getHotelById: getHotelById,
-    createHotel: createHotel
+    createHotel: createHotel,
+    deleteHotel: deleteHotel
 };
