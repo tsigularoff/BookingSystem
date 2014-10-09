5b744565fcf1e48a405cec8d72c186f6169588f6
Kiril Tsarnakliyski <elinos@gmail.com>
2014-10-10 01:13:32 +0300

    One more test and npm test command

--- server/tests/controllers/hotels.spec.js
+++ server/tests/controllers/hotels.spec.js
@@ -47,6 +47,41 @@ describe('HotelsController', function () {
                 expected.push(result);
                 hotelsController.getAllHotels(req, res);
             });
+        });
+        it('should find show the whole information for the hotel', function (done) {
+            var req = {};
+            req.query = {};
+            var res = {};
+            res.send = function (collection) {
+                for (var i = 0; i < collection.length; i++) {
+                    expect(expected[i]._id).to.eql(collection[i]._id);
+                    expect(expected[i].name).to.eql(collection[i].name);
+                    expect(expected[i].description).to.eql(collection[i].description);
+                    expect(expected[i].address).to.eql(collection[i].address);
+                    expect(expected[i].city).to.eql(collection[i].city);
+                    expect(expected[i].star_rating).to.eql(collection[i].star_rating);
+                    expect(expected[i].pictureUrl).to.eql(collection[i].pictureUrl);
+                    expect(expected[i].owner).to.eql(collection[i].owner);
+                    expect(expected[i].usersWithRates).to.eql(collection[i].usersWithRates);
+                    expect(expected[i].userRating).to.eql(collection[i].userRating);
+                }
+                done();
+            };
+            var expected = [];
+            Hotel.create({
+                name: 'TestHotel',
+                description: 'test desc',
+                star_rating: 5,
+                address: 'Center',
+                city: 'Sofia',
+                pictureUrl: 'http://bucharest-guide.ro/poze/76b4d89b45fb70dc02ddf8aa4f077585.jpg',
+                owner: 'test',
+                usersWithRates: [],
+                userRating: 0
+            }, function (err, result) {
+                expected.push(result);
+                hotelsController.getAllHotels(req, res);
+            });
         })
     })
 });
